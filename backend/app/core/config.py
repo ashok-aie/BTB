a5c409cce0706c494ae0d5d43fcb8d660cf54bab
Ashok <53231377+ashok-aie@users.noreply.github.com>
2025-10-23 17:28:39 -0400

    Add SECRET_KEY to Settings class

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,6 +5,7 @@ import os
 class Settings(BaseSettings):
     DATABASE_URL: Optional[str] = None
     DATABASE_DIRECT_URL: Optional[str] = None
+    SECRET_KEY: Optional[str] = None
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
